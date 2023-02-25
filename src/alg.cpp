455f67105b657b0281a52cdee534d4c205d6b036
mellyexe <125124784+mellyexe@users.noreply.github.com>
2023-02-26 01:09:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,13 +5,14 @@
 
 bool checkPrime(uint64_t value) {
 if (value <= 1) return false;
-  if (value == 2) return true;
+  
+if (value == 2) return true;
 
-    if ((value & 1) == 0) return false;
+if ((value & 1) == 0) return false;
 
-    for (uint64_t i = 3; i <= sqrt(value); i += 2) {
-      if (value % i == 0) return false;
-    }
+  for (uint64_t i = 3; i <= sqrt(value); i += 2) {
+    if (value % i == 0) return false;
+  }
 
   return true;
 }
