cfcd484527f3d3864cb576ccdc58e70114b04a60
mellyexe <125124784+mellyexe@users.noreply.github.com>
2023-02-25 23:09:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,46 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+if (value <= 1) return false;
+	if (value == 2) return true;
+
+	if ((value & 1) == 0) return false;
+
+	for (uint64_t i = 3; i <= sqrt(value); i += 2) {
+		if (value % i == 0) return false;
+	}
+
+	return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+	uint64_t counter = 2;
+	uint64_t i = 0;
+
+	if (n == 0) return 0;
+	if (n == 1) return 2;
+
+
+	for (i = 5; counter < n; i += 2) {
+		if (checkPrime(i)) counter++;
+	}
+
+	return i-2;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+
+	for (uint64_t i = value+1; ; i++) {
+		if (checkPrime(i)) return i;
+	}
+
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+	uint64_t sum = 0;
+
+	for (uint64_t i = 2; i < hbound; i++) {
+		if (checkPrime(i)) sum += i;
+	}
+	return sum;
 }
